64cdaa59370ae08da18f32e69d24c5f3ad06164a
Zu-shi <accountistaken@gmail.com>
2023-07-07 06:53:23 -0400

    Finished 5.17

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,17 @@
 describe('Note app', function () {
-  it('front page can be opened', function () {
+
+  beforeEach('Remove users', () => {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('blogs')
   })
 })
